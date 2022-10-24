429177eb307af17dce3ca8e38e28864e6c59b02e
lowage24 <114925366+lowage24@users.noreply.github.com>
2022-10-24 15:26:43 +0300

    Add files via upload

---
+++ lesson 11/assets/js/main1.js
@@ -0,0 +1,122 @@
+// alert('Я, JavaScript!')
+// console.log('Я, JavaScript!')
+
+
+// let name = prompt('Ваше имя', 'Саша');
+// alert(`Ваше имя ${name}`);
+
+
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x)
+
+// let a = prompt('Первое число');
+// let b = prompt('Второе число');
+
+// alert(+a + +b)
+
+1// let num = 3;
+// alert(num);
+
+2// let a = 10;
+// let b = 2;
+
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a % b);
+
+3// let c = 15;
+// let d = 2;
+// let result = (c + d);
+
+// alert(result);
+
+4// let a = 10;
+// let b = 2;
+// let c = 5;
+
+// alert(a + b +c);
+
+5// let a = 17;
+// let b = 10;
+// let d = 7;
+
+// let c = (a - b);
+
+// let result = (d + c);
+
+// alert(result);
+
+// Работа со строками
+
+1// let str = ('Привет, Мир!');
+// alert(str);
+
+2// let str1 = ('Привет,');
+// let str2 = ('Мир!');
+
+// alert(str1 + str2);
+
+3// let name = Дмитрий;
+// alert(`Привет, ${name});
+
+4// let age = 18
+// alert(`Ваш возраст, ${age}`);
+
+// Функция prompt
+
+1// let name = prompt('Ваше имя', 'Дмитрий');
+// alert(`Привет, ${name}`);;
+
+2// let num = prompt('Введите число');
+// alert(num ** 2);
+
+// Обращение к символам строки
+
+1// let str = ('abcde');
+
+// alert(str[0]);
+// alert(str[2]);
+// alert(str[5]);
+
+2// let num = ('12345');
+// alert(num[0]*num[1]*num[2]*num[3]*num[4]);
+
+// Практика
+1// let s = 60;
+// let h = s**3;
+// let day = h*24;
+// let mouth = day*30;
+// alert(h);
+// alert(day);
+// alert(mouth);
+
+2// let time = new Date();
+// console.log(time.getHours(), time.getMinutes(), time.getSeconds());
+
+3// let num = prompt();
+// alert(num**2);
+
+// Работа с присваиванием и декрементами //
+
+1// var num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+
+2// var num = 10;
+// num ++ ;
+// num ++ ;
+// num -- ;
+// alert(num);
---
+++ lesson 11/assets/js/main2.js
@@ -0,0 +1,103 @@
+// Задача 1
+
+// let a = prompt('число а равно', 10);
+// let result = (a == 10) ? true : false;
+// alert(result)
+
+
+// Задача 2
+
+
+// let min = 45;
+
+// let result = (min < 15) ? 'one':
+//     (min < 30) ? 'two':
+//     (min < 45) ? 'three':
+//     (min < 60) ? 'four':
+//     'Введите число от 0 до 59';
+// alert(result)
+
+//  Задача 3
+// let lang = 'en'
+
+// let arr = null
+// switch(lang){
+//     case 'ru':
+//         arr = ['Пнедельник' , 'Вторник', 'Среда','Четверг','Пятница','Суббота','Воскресенье']
+//         alert(arr)
+//         break
+//     case 'en' :
+//         arr = ['Sunday','Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday']
+//         alert(arr)
+//         break
+// }
+
+
+// Задача 4
+// let a = 1
+
+
+
+// if (a == 0){
+//     alert('True')
+// }else{
+//     alert(false)
+// }
+
+// if (a>0){
+//     alert('Верно')
+// }else{
+//     alert('Неверно')
+// }
+
+// if (a<0){
+//     alert('Верно')
+// }else{
+//     alert('Неверно')
+// }
+
+// if (a>=0){
+//     alert('Верно')
+// }else{
+//     alert('Неверно')
+// }
+
+// if (a<=0){
+//     alert('Верно')
+// }else{
+//     alert('Неверно')
+// }
+
+// if (a!=0){
+//     alert('Верно')
+// }else{
+//     alert('Неверно')
+// }
+
+
+// if (a=='test'){
+//     alert('Верно')
+// }else{
+//     alert('Неверно')
+// }
+
+
+// if (a  === '1'){
+//     alert('верно')
+// }else{
+//     alert('Неверно')
+// }
+
+// работа с && и ||
+
+// let a = 0
+
+// if (a >0 && a< 5){
+//     alert(true)
+// }else{
+//     alert(false)
+// }
+
+if (a ==0  a == 2){
+    a += 7
+}
